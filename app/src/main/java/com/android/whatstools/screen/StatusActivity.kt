7366c99054c309838bb4a5883ec795ad73344a22
venkat-clone <lingampally.venkey@gmail.com>
2022-06-05 22:58:37 +0530

    -1.0.1

--- app/src/main/java/com/android/whatstools/screen/StatusActivity.kt
+++ app/src/main/java/com/android/whatstools/screen/StatusActivity.kt
@@ -3,26 +3,35 @@ package com.android.whatstools.screen
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import androidx.lifecycle.ViewModelProvider
 import com.android.whatstools.R
-import com.android.whatstools.screen.status.statusList
+import com.android.whatstools.screen.status.MediaFragment
+import com.android.whatstools.screen.status.StatusList
+import com.android.whatstools.screen.status.StatusViewModel
+import com.android.whatstools.screen.status.statusAdapter
 
 class StatusActivity : AppCompatActivity() {
-    val fm:FragmentManager = supportFragmentManager
-    private val mainFragment:Fragment = statusList()
+    private val fm:FragmentManager = supportFragmentManager
+    private val mainFragment:Fragment = StatusList()
+    private val medialFragment:Fragment = MediaFragment()
+    private lateinit var  viewModel:StatusViewModel;
+    private var current:Fragment? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_status)
+        viewModel = ViewModelProvider(this)[StatusViewModel::class.java]
         handleIntent(intent);
-
+        observers()
 
 
     }
 
     private fun  handleIntent(intent:Intent){
-        fm.beginTransaction().add(R.id.layout,mainFragment).commit()
-
+        setFragment(mainFragment)
         if (intent.hasExtra("path")){
             val path:String? = intent.getStringExtra("path")
             if(path!!.contains(".mp4")){
@@ -33,4 +42,47 @@ class StatusActivity : AppCompatActivity() {
             }
         }
     }
+
+    fun observers(){
+        viewModel.status.observe(this){
+            if (it!=null){
+                setFragment(medialFragment)
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+        if (current!!.equals(mainFragment))
+            super.onBackPressed()
+        else
+            removeFragment()
+
+    }
+
+    fun setFragment(fragment:Fragment ){
+        if(current != null) fm.beginTransaction().hide(current!!).commit()
+        fm.beginTransaction().add(R.id.layout,fragment).commit()
+        current = fragment
+    }
+    fun removeFragment(){
+        if(current!!.equals(mainFragment)) finish()
+        else {
+            fm.beginTransaction().remove(current!!).show(mainFragment).commit()
+            current = mainFragment
+        }
+    }
+
+
+
+    fun whatsappShare(view: View){
+        val intent:Intent = Intent(Intent.ACTION_SEND)
+
+    }
+    fun Download(view: View){
+
+    }
+    fun Share(view: View){
+
+    }
+
 }
--- app/src/main/java/com/android/whatstools/screen/status/statusList.kt
+++ app/src/main/java/com/android/whatstools/screen/status/statusList.kt
@@ -1,29 +1,66 @@
 package com.android.whatstools.screen.status
 
-import androidx.lifecycle.ViewModelProvider
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.media.ThumbnailUtils
+import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.Environment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.android.whatstools.R
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.android.whatstools.databinding.FragmentStatusListBinding
+import java.io.File
 
-class statusList : Fragment() {
 
-    companion object {
-        fun newInstance() = statusList()
-    }
+class StatusList : Fragment() {
+    lateinit var binding : FragmentStatusListBinding;
+    lateinit var statusViewModel: StatusViewModel;
 
     private lateinit var viewModel: StatusListViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-
-        return inflater.inflate(R.layout.fragment_status_list, container, false)
+        binding = FragmentStatusListBinding.inflate(inflater,container,false)
+        binding.lifecycleOwner = this
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(StatusListViewModel::class.java)
+        viewModel = ViewModelProvider(this)[StatusListViewModel::class.java]
+        statusViewModel = ViewModelProvider(requireActivity())[StatusViewModel::class.java]
+        Observers()
     }
+
+
+    fun Observers(){
+        binding.toolbar.setNavigationOnClickListener {
+            requireActivity().finish()
+        }
+        viewModel.bitmapList.observe(viewLifecycleOwner, Observer {
+            if(it!=null){
+                val adapter = statusAdapter(requireContext(), it,viewModel)
+                binding.recycler.adapter = adapter
+            }
+        })
+        viewModel.status.observe(viewLifecycleOwner) {
+            if (it != null) {
+                statusViewModel.status.postValue(it)
+//                requireFragmentManager().beginTransaction().add(MediaFragment(),"").commit()
+            }
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+
+
+
 
 }
