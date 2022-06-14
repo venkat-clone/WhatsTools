6aeffed2422508e9275afa53819274dfa39c0d52
venkat-clone <lingampally.venkey@gmail.com>
2022-06-14 15:46:02 +0530

    A

--- app/src/main/java/com/android/whatstools/screen/MagicTextActivity.kt
+++ app/src/main/java/com/android/whatstools/screen/MagicTextActivity.kt
@@ -1,8 +1,13 @@
 package com.android.whatstools.screen
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import com.android.whatstools.databinding.ActivityMagicTextBinding
 import com.android.whatstools.screen.magictext.MagicTextViewModel
@@ -20,7 +25,7 @@ class MagicTextActivity : AppCompatActivity() {
         viewModel = ViewModelProvider(this)[MagicTextViewModel::class.java]
         binding.viewmodel = viewModel
         setContentView(binding.root)
-
+        binding.clickHandler = ClickHandler()
 
         observer()
 
@@ -40,6 +45,30 @@ class MagicTextActivity : AppCompatActivity() {
             viewModel.Genaratetext()
         }
 
+    }
+    inner class ClickHandler{
+        fun Copy(){
+            val  service = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val clip = ClipData.newPlainText("copid",viewModel.text.value)
+            service.setPrimaryClip(clip)
+            Toast.makeText(baseContext,"Copied to Clipboard",Toast.LENGTH_SHORT).show()
+        }
+        fun WhatsappShare(){
+            val intent=Intent(Intent.ACTION_SEND)
+            intent.`package` = "com.whatsapp"
+            intent.type = "text/plain"
+            intent.putExtra(Intent.EXTRA_TEXT,viewModel.text.value)
+            startActivity(intent)
+        }
+        fun Send(){
+            val intent=Intent(Intent.ACTION_SEND)
+            intent.type = "text/plain"
+            intent.putExtra(Intent.EXTRA_TEXT,viewModel.text.value)
+            startActivity(intent)
+        }
+        fun Refresh(){
+            viewModel.Genaratetext()
+        }
     }
 
 }
--- app/src/main/java/com/android/whatstools/screen/magictext/MagicTextViewModel.kt
+++ app/src/main/java/com/android/whatstools/screen/magictext/MagicTextViewModel.kt
@@ -1,8 +1,10 @@
 package com.android.whatstools.screen.magictext
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.android.whatstools.utlis.BaseClass
+import kotlin.math.log
 import kotlin.random.Random
 
 class MagicTextViewModel:ViewModel() {
@@ -22,16 +24,17 @@ class MagicTextViewModel:ViewModel() {
 
     fun Genaratetext(){
         var Ttext =""
-        for (v in 1..15) {
-            val c1 = Random.nextInt(0, 6)
+        for (v in 1..31) {
+            val c1 = if(v!=1) Random.nextInt(0, 10) else 0
             val c2 = Random.nextInt(c1, 20)
-            val c3 = Random.nextInt(c2, 27)
-            if(v%8==0)
-                Ttext+="${" ".repeat(c1)}${mainText.value}\n"
+            val c3 = Random.nextInt(c2, 30)
+            Log.i("Log_1","$c3")
+            if(v%16==0)
+                Ttext+="${" ".repeat(c1)}${mainText.value}"
             else
-                Ttext+="${" ".repeat(c1)}${starText.value}${" ".repeat(c2)}${dotText.value}${" ".repeat(c3)}${starText.value}\n"
+                Ttext+="${" ".repeat(c1)}${starText.value}${" ".repeat(c2)}${dotText.value}${" ".repeat(c3)}${starText.value}"
         }
-        text.value = Ttext
+        text.value = Ttext.trimStart()
 
     }
 
